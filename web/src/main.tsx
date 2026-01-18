e33b4f890d50db25fd20c4e9d21ae12ac5567c3e
Nicholas Neto <nicholaspereiraneto@gmail.com>
2026-01-18 22:03:25 +0000

    added main fix

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,48 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles/global.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// const queryClient = new QueryClient()
+
+// createRoot(document.getElementById("root")!).render(
+//   <App />
+// )
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <QueryClientProvider client={queryClient}>
+//     <App />
+//   </QueryClientProvider>
+// )
+
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+// const queryClient = new QueryClient()
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <QueryClientProvider client={queryClient}>
+//     <App />
+//   </QueryClientProvider>
+// )
+
+
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
-)
+  </QueryClientProvider>
+);
